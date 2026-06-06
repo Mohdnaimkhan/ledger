49d3f45122dff6bc9b6c203fc6657fbeb77c03e6
mohdnaimkhan <mohdnaeem789@gmail.com>
2026-06-06 09:08:40 +0530

    update ui add navbar add busenes name

--- src/main/java/com/naim/ledger/entity/Customer.java
+++ src/main/java/com/naim/ledger/entity/Customer.java
@@ -18,6 +18,7 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String shopName;
     private String firstName;
 
     private String lastName;
